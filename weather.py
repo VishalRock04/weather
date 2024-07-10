f86d7764f6bb472d3e7bac6a9c5f4ec671ab0c75
Vishal Kumar <133562727+VishalRock04@users.noreply.github.com>
2024-07-10 21:22:59 +0530

    Update and rename wea.py to weather.py

--- weather.py
+++ weather.py
@@ -5,10 +5,10 @@ import requests
 def city_keys():
   city = city_name.get()
   data = requests.get('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=d78f79a69d66be56aeb228cfe35bc552').json()
-  pressure1.config(text=data['main']['pressure'])
-  temp1.config(text=str(data['main']['temp']-273.15))
-  climate1.config(text=data['weather'][0]['description'])
-
+  pressure.config(text=data['main']['pressure'])
+  temp.config(text=str(data['main']['temp']-273.15))
+  climate.config(text=data['weather'][0]['description'])
+  humidity.config(text=data['main']['humidity'])
 
 def window_exit():
   wea.destroy()
@@ -28,27 +28,29 @@ list_name = ["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh
 com =ttk.Combobox(wea,text='WEATHER',values = list_name,font=('Times',10,'bold'),textvariable=city_name)
 com.place(x=155,y=100,height=30,width=200)
 
-temp = Label(wea,text='TEMPRATURE',font=('Times',13))
-temp.place(x = 100,y =210,height=30,width=112)
+Label(wea,text='TEMPRATURE',font=('Times',13)).place(x = 100,y =210,height=30,width=112)
 
-temp1 = Label(wea,text='',font=('Times',13))
-temp1.place(x = 300,y =210)
+Label(wea,text='PRESSURE',font=('Times',13)).place(x = 100,y =250,height=30,width=112)
 
-pressure = Label(wea,text='PRESSURE',font=('Times',13))
-pressure.place(x = 100,y =250,height=30,width=112)
+Label(wea,text='WEATHER',font=('Times',13)).place(x = 100,y =290,height=30,width=112)
 
-pressure1 = Label(wea,text='',font=('Times',13))
-pressure1.place(x = 300,y =250)
+Label(wea,text=',HUMIDITY',font =('Times',13)).place(x=100,y=330,height=30,width=112)
 
-climate = Label(wea,text='WEATHER',font=('Times',13))
-climate.place(x = 100,y =290,height=30,width=112)
+temp = Label(wea,text='',font=('Times',13))
+temp.place(x = 300,y =210)
 
-climate1 = Label(wea,text='',font=('Times',13))
-climate1.place(x = 300,y =290)
+pressure = Label(wea,text='',font=('Times',13))
+pressure.place(x = 300,y =250)
 
-button1 = Button(text='DONE',font=('Times',20,'bold'),bg='yellow',command=city_keys)
-button1.place(x = 200,y = 150,height=40,width=100)
+climate = Label(wea,text='',font=('Times',13))
+climate.place(x = 300,y =290)
 
-button2 = Button(text='EXIT',font=('Times',20,'bold'),bg='red',command=window_exit).place(x=200,y=360,height=40,width=100)
+humidity =Label(wea,text='',font=('Times',13))
+humidity.place(x=300,y=330)
+
+DONE_button = Button(text='DONE',font=('Times',20,'bold'),bg='yellow',command=city_keys).place(x = 200,y = 150,height=40,width=100)
+
+
+EXIT_button = Button(text='EXIT',font=('Times',20,'bold'),bg='red',command=window_exit).place(x=200,y=420,height=40,width=100)
 
 wea.mainloop()
